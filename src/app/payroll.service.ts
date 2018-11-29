d211bc515580a2691802299e648f14eb98172580
Robert Campbell <robertgcampbell95@gmail.com>
2018-11-29 00:00:21 -0800

    Add payroll service

---
+++ src/app/payroll.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+export interface DBResponse {
+  data: any;
+  resp: any;
+}
+
+@Injectable()
+export class PayrollService {
+  constructor(private http: HttpClient) { }
+
+  salaryHistory(id?: string) {
+    return this.http.get<DBResponse>(`${environment.api}/salary?id=${id}`);
+  };
+
+  user(id: string) {
+    return this.http.get<DBResponse>(`${environment.api}/employee/${id}`);
+  }
+
+  addSalary(id: string, salary: string) {
+    return this.http.post<DBResponse>(`${environment.api}/salary/${id}`, {salary});
+  }
+};

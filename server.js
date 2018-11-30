165b3c3a8d1aed29c5554b87d4fd0ae7bdf881b4
Robert Campbell <robertgcampbell95@gmail.com>
2018-11-30 01:26:21 -0800

    Split server from Twitter App

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mysql = require('mysql');
+
+dotenv.config();
+
+const app = express();
+
+const con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS
+  });
+  
+  con.connect(err => {
+    if(err) throw err;
+    console.log("Connected!");
+    
+    // Setting up the database
+    con.query("use employees", (err, result) => {
+      if (err) throw err;
+    });
+  
+  });
+  
+  app.use(require('cors')());
+  
+  //this is needed to get POST parameters
+  app.use(require('body-parser').json());
+
+  // APIs
+  
+  app.get('/api/salary', (req, res) => {
+    let query = "select * from salaries where emp_no = " + req.query.id; 
+    con.query(query, (err, result) => {
+      if (err) throw err;
+      
+      res.send(result);
+      console.log("/api/salary/" + req.params.id);
+    });
+  });
+  
+  app.get('/api/employee/:id', (req, res) => {
+    let query = "select * from employees where emp_no = " + req.params.id;  
+    con.query(query, (err, result) => {
+      if (err) throw err;
+  
+      res.send(result);
+    })
+  });
+  
+  app.get('/api/employee', (req, res) => {
+    let query = "select * from employees where last_name like '%" + req.query.q + "%'";  
+    con.query(query, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    })
+  });
+
+  app.listen(5000, () => console.log('Server running'));

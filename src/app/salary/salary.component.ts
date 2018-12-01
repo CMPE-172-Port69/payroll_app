a6a8da24578e957bf0a8d0656d939279a02f247d
Yu Ning Hee <yuninghee@gmail.com>
2018-12-01 00:30:33 -0800

    Get Salary History Code

--- src/app/salary/salary.component.ts
+++ src/app/salary/salary.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PayrollService } from "../payroll.service";
 
 @Component({
-  selector: 'app-salary',
-  templateUrl: './salary.component.html',
-  styleUrls: ['./salary.component.css']
+  selector: "app-salary",
+  templateUrl: "./salary.component.html",
+  styleUrls: ["./salary.component.css"],
+  providers: [PayrollService]
 })
 export class SalaryComponent implements OnInit {
+  inputID: string = "";
+  stringInvalid: boolean = true;
 
-  constructor() { }
+  salaries: any;
 
-  ngOnInit() {
+  constructor(private payroll: PayrollService) {}
+
+  ngOnInit() {}
+
+  checkIDString(input: string) {
+    // Validation input
+    // Input must be a 5 digit number starting with a 1.
+    let reg = /[1-9][0-9]{4}/;
+    let expFound = reg.test(input);
+
+    try {
+      if (input.length === 5 && expFound) {
+        this.stringInvalid = false;
+      } else if (input.length !== 5 || !expFound) {
+        this.stringInvalid = true;
+      }
+    } catch (err) {
+      this.stringInvalid = true;
+      console.log("Input string is invalid");
+    }
   }
 
+  onIDSearch() {
+    this.payroll.salaryHistory(this.inputID).subscribe(response => {
+      try {
+        this.salaries = response;
+        this.inputID = "";
+      } catch (err) {
+        window.alert("Invalid uID entered.");
+      }
+    });
+  }
 }

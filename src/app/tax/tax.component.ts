3b3e907826ed20011c5523a1b04c489ef09c5436
Robert Campbell <robertgcampbell95@gmail.com>
2018-12-01 23:01:04 -0800

    Add text field and button

--- src/app/tax/tax.component.ts
+++ src/app/tax/tax.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { PayrollService } from '../payroll.service';
 
 @Component({
   selector: 'app-tax',
   templateUrl: './tax.component.html',
-  styleUrls: ['./tax.component.css']
+  styleUrls: ['./tax.component.css'],
+  providers: [PayrollService]
 })
 export class TaxComponent implements OnInit {
 
-  constructor() { }
+  inputID: string = "";
+
+  stringInvalid: boolean = true;
+
+  constructor(private payroll: PayrollService) { }
 
   ngOnInit() {
   }
 
+  checkIDString(input: string){
+    // Validation input
+    // Input must be a 5 digit number starting with a 1.
+    let reg = /[1-9][0-9]{4}/
+    let expFound = reg.test(input);
+
+    try {
+      if( input.length === 5 && expFound){
+        this.stringInvalid = false;
+      }
+      else if(input.length !== 5 || !expFound) {
+        this.stringInvalid = true;
+      }
+    }
+    catch(err) {
+      this.stringInvalid = true;
+      console.log("Input string is invalid");
+    }
+
+  }
+
+  generateW2() {
+    ;
+  }
 }

2400b274248b6d233120d69bed3533e2e1c75c37
Robert Campbell <robertgcampbell95@gmail.com>
2018-12-02 16:42:45 -0800

    Implement method to find calculate tax bracket of employee

--- src/app/tax/tax.component.ts
+++ src/app/tax/tax.component.ts
@@ -10,6 +10,19 @@ import { PayrollService } from '../payroll.service';
 export class TaxComponent implements OnInit {
 
   inputID: string = "";
+  mostRecentSalary: any;
+  salaries: any;
+  employee: any;
+
+  search: boolean = false;
+
+  bracket: number;
+  taxes: {
+    incomeTax: any,
+    ssTax: any,
+    medicareTax
+  }
+
 
   stringInvalid: boolean = true;
 
@@ -40,6 +53,57 @@ export class TaxComponent implements OnInit {
   }
 
   generateW2() {
-    ;
+    this.payroll.user(this.inputID).subscribe(response => {
+      this.employee = response[0];
+      console.log(response);
+    });
+
+    this.payroll.salaryHistory(this.inputID).subscribe(response => {
+      console.log(JSON.stringify(response));
+      this.salaries = response;
+      this.mostRecentSalary = this.salaries[this.salaries.length - 1];
+
+      this.getBracket();
+    });
+
+    this.search = true;
+    this.getBracket();
+  }
+
+  getBracket() {
+    let salary = this.mostRecentSalary.salary;
+    salary = parseInt(salary);
+
+    let brackets = [
+      {amount: 0 , percent: .10},
+      {amount: 9525 , percent: .12},
+      {amount: 38700 , percent: .22},
+      {amount: 82500 , percent: .24},
+      {amount: 157500 , percent: .32},
+      {amount: 200000 , percent: .35},
+      {amount: 500000, percent: .37}
+  ]
+    if(salary < brackets[1].amount) {
+      this.bracket = brackets[0].percent;
+    }
+    else if(salary < brackets[2].amount) {
+      this.bracket = brackets[1].percent;
+    }
+    else if(salary < brackets[3].amount) {
+      this.bracket = brackets[2].percent;
+    }
+    else if(salary < brackets[4].amount) {
+      this.bracket = brackets[3].percent;
+    }
+    else if(salary < brackets[5].amount) {
+      this.bracket = brackets[4].percent;
+    }
+    else if(salary < brackets[6].amount) {
+      this.bracket = brackets[5].percent;
+    }
+    else {
+      this.bracket = brackets[6].percent;
+    }
+    this.taxes.incomeTax = salary * this.bracket;
   }
 }
